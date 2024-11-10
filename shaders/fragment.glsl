aa522d428d280c55d2c8994df4748c9a3045dd1d
Vitaliy Belikov <vital@qwe.lv>
2024-11-10 22:32:28 +0200

    aspect ratio 1:1

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -4,10 +4,21 @@ uniform float u_Time;
 in vec4 v_Color;
 out vec4 f_Color;
 void main() {
-    vec2 uv = gl_FragCoord.xy / u_FramebufferSize;
+    vec2 uv = gl_FragCoord.xy;
+    if (u_FramebufferSize.x > u_FramebufferSize.y) {
+        uv.xy /= u_FramebufferSize.y;
+        uv.x -= 0.5 * (u_FramebufferSize.x / u_FramebufferSize.y);
+        uv.y -= 0.5;
+    } else {
+        uv.xy /= u_FramebufferSize.x;
+        uv.x -= 0.5;
+        uv.y -= 0.5 * (u_FramebufferSize.y / u_FramebufferSize.x);
+    }
     vec4 color = v_Color;
     color.r = sin(u_Time) * 0.5 + 0.5;
     color.b = uv.x;
     color.g = uv.y;
+    float d = length(uv);
+    color.r = smoothstep(0.498, 0.502, d) * color.r;
     f_Color = color;
 }
