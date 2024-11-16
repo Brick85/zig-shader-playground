7315caa585a46120ab2d740c68dab74853ad7804
Vitaliy Belikov <vital@qwe.lv>
2024-11-16 10:03:37 +0200

    colors and structs. camera move only in one plane.

--- shaders/fragment.glsl
+++ shaders/fragment.glsl
@@ -8,21 +8,29 @@ uniform float u_Time;
 in vec4 v_Color;
 out vec4 f_Color;
 
+struct Material {
+    Color color;
+};
+
 struct Light {
     vec3 position;
+    Color color;
 };
 
 struct Sphere {
-    vec3 position;
     float radius;
-    Color color;
 };
 
-struct Ground {
-    float height;
-    Color color;
+struct Object {
+    vec3 position;
+    Material material;
+    int objectTypeIndex;
+    bool isSphere;
+    bool isGround;
 };
 
+Sphere[3] spheres;
+Object[4] objects;
 
 float map(float value, float min1, float max1, float min2, float max2) {
     float perc = (value - min1) / (max1 - min1);
@@ -31,11 +39,13 @@ float map(float value, float min1, float max1, float min2, float max2) {
     return ret;
 }
 
-float distToSphere(vec3 pos, Sphere sphere) {
-    return length(pos - sphere.position) - sphere.radius;
+float distToSphere(vec3 pos, Object obj) {
+    Sphere sphere = spheres[obj.objectTypeIndex];
+    return length(pos - obj.position) - (sphere.radius);
 }
 
-float distToGround(vec3 pos, float ground) {
+float distToGround(vec3 pos, Object obj) {
+    float ground = obj.position.x;
     return pos.y - ground;
 }
 
@@ -51,52 +61,75 @@ void main() {
         uv.y -= 0.5 * (u_FramebufferSize.y / u_FramebufferSize.x);
     }
 
+    objects[0] = Object(
+            vec3(-1.0, 0, 0),
+            Material(Color(0.5, 0.5, 0.5)),
+            0,
+            false,
+            true
+        );
+
     Color ambient = Color(.0);
 
-    Light baseLight;
-    baseLight.position = vec3(-2.0, 10.0, -2.0);
+    spheres[0] = Sphere(1.0);
+    spheres[1] = Sphere(0.5);
+    spheres[2] = Sphere(0.3);
 
-    Ground ground;
-    ground.height = -1.0;
-    ground.color = Color(0.5, 0.5, 0.5);
+    objects[1] = Object(
+            vec3(0, 0, -3.0),
+            Material(Color(1, 0, 0)),
+            0,
+            true,
+            false
+        );
 
-    Sphere[3] spheres;
-    spheres[0] = Sphere(vec3(0.0, 0.0, -3.0), 1.0, Color(1., sin(u_Time), 0.));
-    spheres[1] = Sphere(vec3(1.5, -0.5, -4.0), 0.5, Color(0., 1., cos(u_Time*3.)));
-    spheres[2] = Sphere(vec3(-1.5, -0.7, -4.0), 0.3, Color(sin(u_Time*10.), 0., 1.));
+    objects[2] = Object(
+            vec3(1.5, -0.5, -4.0),
+            Material(Color(0, 1, 0)),
+            1,
+            true,
+            false
+        );
+    objects[3] = Object(
+            vec3(-1.5, -0.7, -4.0),
+            Material(Color(0, 0, 1)),
+            2,
+            true,
+            false
+        );
 
-
+    vec3 test = vec3(sin(u_Time), cos(u_Time / 2.) * 0.9, 0.0);
     vec3 cam = vec3(0., 0., 1.);
-    vec3 vp = vec3(uv, 0.);
-    vec3 ray = normalize(vp - cam);
-    
+    vec3 vp = vec3(uv, 0.0);
 
-    vec3 pos = cam;
+    vec3 ray = normalize(vp - cam);
+    vec3 pos = cam + test;
+
     Color obj_color = ambient;
     for (int i = 0; i < MAX_STEPS; i++) {
         float dist = 1.;
-        
-        for (int j = 0; j < 3; j++) {
-            float objDist = distToSphere(pos, spheres[j]);
+
+        for (int j = 0; j < 4; j++) {
+            Object obj = objects[j];
+            float objDist = 0.;
+            if (obj.isGround) {
+                objDist = distToGround(pos, obj);
+            } else if (obj.isSphere) {
+                objDist = distToSphere(pos, obj);
+            }
             if (objDist < dist) {
                 dist = objDist;
-                obj_color = spheres[j].color;
+                obj_color = obj.material.color;
             }
         }
-        float objDist = distToGround(pos, ground.height);
-        if (objDist < dist) {
-            dist = objDist;
-            obj_color = ground.color;
-        }
 
-        if(dist < MIN_DIST) break;
+        if (dist < MIN_DIST) break;
         pos += ray * dist;
         obj_color = ambient;
     }
     Color color;
     float depth = map(length(pos), 8., 2., 0., 1.);
     color = obj_color * depth;
-    // color = Color();
 
     f_Color = vec4(color, 1.0);
 }
